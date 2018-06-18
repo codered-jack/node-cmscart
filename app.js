32ab71bfee9a631095b7f92ea573038aecd99513
codered-jack <sumitnbq@gmail.com>
2018-06-19 00:40:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,106 @@
+var express=require('express');
+var path=require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+
+//connect to DB
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  		console.log('connected to mongodb');
+});
+
+//Init
+
+var app=express();
+
+//View engine setup
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+//Set public folder
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// Set global errors variable
+app.locals.errors = null;
+
+// Body Parser middleware
+// 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Express Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+//  cookie: { secure: true }
+}));
+
+
+
+// Express Validator middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+                , root = namespace.shift()
+                , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    },
+    customValidators: {
+        isImage: function (value, filename) {
+            var extension = (path.extname(filename)).toLowerCase();
+            switch (extension) {
+                case '.jpg':
+                    return '.jpg';
+                case '.jpeg':
+                    return '.jpeg';
+                case '.png':
+                    return '.png';
+                case '':
+                    return '.jpg';
+                default:
+                    return false;
+            }
+        }
+    }
+}));
+
+
+
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Set routes 
+var pages = require('./routes/pages.js');
+var adminPages = require('./routes/admin_pages.js');
+
+app.use('/', pages);
+app.use('/admin/pages', adminPages);
+//Start the server
+
+var port =3000;
+app.listen(port,function(){
+	console.log('Server started on port'+ port);
+});
